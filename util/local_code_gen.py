bc4e9c855a22d2e70490d7867f954d08c5138c3b
hejiangyuan <hejiangyuan@daoshituan.com>
2022-05-18 21:17:27 +0800

    完善代码生成模板

--- util/local_code_gen.py
+++ util/local_code_gen.py
@@ -3,6 +3,8 @@
 import os
 
 # 需要安装 pyyaml 模块
+import time
+
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
@@ -79,7 +81,7 @@ class LocalCodeGen:
                 template_flag = template[2] if len(template) > 2 else ''
                 result_file = source_root + '\\' + template_output
 
-                if template_flag.__contains__('table_name=') and not template_flag.__contains__('table_name='+table_name):
+                if template_flag.__contains__('table_name=') and not template_flag.__contains__(table_name):
                     continue
 
                 env = Environment(loader=FileSystemLoader(template_root))
@@ -98,19 +100,26 @@ class LocalCodeGen:
             fields_setting = yaml.load(f, Loader=yaml.CLoader)
 
         fields = []
+        last_field_name = ''
         for field in fields_setting['fields']:
             field_name = field[0]
-            field_type = field[1] or ''
+            field_db_type = field[1] or ''
             comment = field[2]
             flag = field[3] if len(field) > 3 else ''
+            default_value = field[4] if len(field) > 4 else None
+
             field_length = ''
 
-            if field_type.__contains__('('):
-                field_length = field_type[field_type.index('(')+1:field_type.index(')')]
-                field_type = field_type[0:field_type.index('(')].lower()
+            if field_db_type.__contains__('('):
+                field_length = field_db_type[field_db_type.index('(')+1:field_db_type.index(')')]
+                field_type = field_db_type[0:field_db_type.index('(')].lower()
+            else:
+                field_type = field_db_type
 
             java_type = ''
             if field_type == 'varchar':
+                java_type = 'String'
+            elif field_type == 'text':
                 java_type = 'String'
             elif field_type == 'int':
                 java_type = 'Integer'
@@ -119,16 +128,34 @@ class LocalCodeGen:
             elif field_type == 'datetime':
                 java_type = 'Date'
 
+            if flag.__contains__('NOT_NULL'):
+                null_default = f"NOT NULL"
+            else:
+                null_default = f"NULL"
+
+            if default_value is not None:
+                null_default = null_default + f" DEFAULT '{default_value}'"
+
+            collate = ''
+            if field_type == 'varchar':
+                collate = "COLLATE 'utf8mb4_general_ci'"
+
             fields.append({
                 'field_name': field_name,
                 'field_name_pascal': pascal_word(field_name),
                 'field_name_camel': camel_word(field_name),
+                'field_db_type': field_db_type,
                 'field_type': field_type,
                 'field_length': field_length,
                 'java_type': java_type,
                 'comment': comment,
-                'flag': flag
+                'flag': flag,
+                'null_default': null_default,
+                'collate': collate,
+                'after_field_name': last_field_name
             })
+
+            last_field_name = field_name
 
         # print(fields)
         # exit()
@@ -154,5 +181,12 @@ if __name__ == '__main__':
     codeGen = LocalCodeGen()
 
     # rpa-cloud 模板
-    src = r'D:\project\rpa\source\rpa-cloud\template_file'
+    # src = r'D:\project\rpa\source\rpa-cloud-codegen\template_file'
+
+    # 招商局融通模板
+    src = r'D:\project\zhuang\source\zsjrt-api-codegen\template_file'
     codeGen.gen_code(src)
+
+    # while True:
+    #     codeGen.gen_code(src)
+    #     time.sleep(5)
